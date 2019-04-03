694296f0abd590ce1119cb572ac4d79e18c911f8
Ryan Pethel <arpethel@gmail.com>
2019-04-03 21:14:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,10 @@ class Song
   # each song has a name, artist, and genre
   # Keep track of the number of songs that it creates
 
+  @@count = 0
+  @@artist = []
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
