f300359d0ef1cd86e9d80334fea750820103e07c
Ryan Pethel <arpethel@gmail.com>
2019-04-03 21:10:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre, :song
 
-  # @@count
-  # @@artists
-  # @@genres
+  # the Song class can produce individual songs
+  # each song has a name, artist, and genre
+  # Keep track of the number of songs that it creates
+
+  def initialize(song)
+    @song = song
+  end
+
+  
 
 end
